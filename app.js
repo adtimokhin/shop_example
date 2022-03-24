71e47891c408af7ba8a35820b799ed63b4ac9575
adtimokhin <adtimokhin@gmail.com>
2022-03-24 19:02:13 +0000

    I have setted a view engine - ejs. I made a sample page that is returend for the URL "/say_hi"

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express();
 // importing routers:
 const globalRouter = require("./routes/globalRoutes.js");
 
+//configuring template view engine
+app.set("views", "views"); // specifing where views live
+app.set("view engine", "ejs");
+
 // setting global variables for all routers
 app.use(express.static(path.join(__dirname, "public"))); // making "public" folder be viewed as static content folder
 app.use(globalRouter);
--- routes/globalRoutes.js
+++ routes/globalRoutes.js
@@ -4,8 +4,7 @@ const router = express.Router();
 // Soon we will add controllers that will map some actions to urls passed in the routers. For now, the two will be combined in here.
 
 router.get("/say_hi", (requesst, response, next) => {
-  console.log("Everything is fine!");
-  next();
+  response.render("index.ejs");
 });
 
 module.exports = router;
